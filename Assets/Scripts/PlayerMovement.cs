cd85109a1f04d0b231ff9ef16da1750e41cf8255
rafatars <rafael.trigo.azevedo@gmail.com>
2024-04-13 18:26:56 -0300

    Camera movement-Now works

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,23 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : CharacterMovement
 {
+    // public override void MoveLogic()
+    // {
+    //     if (Input.GetKey(KeyCode.W))
+    //     {
+    //         MoveUp();
+    //     }
+    //     if (Input.GetKey(KeyCode.S))
+    //     {
+    //         MoveDown();
+    //     }
+    //     if (Input.GetKey(KeyCode.A))
+    //     {
+    //         MoveLeft();
+    //     }
+    //     if (Input.GetKey(KeyCode.D))
+    //     {
+    //         MoveRight();
+    //     }
+    // }
+
+    public float speed = 5.0f;
+    public Transform cam;
+    public float turnSmoothTime = 0.1f;
+    float turnSmoothVelocity;
     public override void MoveLogic()
     {
-        if (Input.GetKey(KeyCode.W))
+        //values between -1 and 1 if the player is pressing the keys 'a', 'd' or the arrow keys
+        float horizontal = Input.GetAxis("Horizontal");
+        //values between -1 and 1 if the player is pressing the keys 'w', 's' or the arrow keys
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, 0, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
         {
-            MoveUp();
+            //angle of the camera in the y axis
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            //smoothly rotate the player to the target angle
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            //rotate the player
+            transform.rotation = Quaternion.Euler(0, angle, 0);
+
+            //set the move direction to the direction the camera is facing
+            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
+
+            //move the player
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.S))
-        {
-            MoveDown();
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            MoveLeft();
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            MoveRight();
-        }
+
     }
 }
